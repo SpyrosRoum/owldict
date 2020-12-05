850213028ca5d042cd5b81ee0153957e38ec0b4d
Spyros Roum <spyrosr1@gmail.com>
2020-12-05 17:38:22 +0200

    Move code out of main and into utils

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod entry;
+mod utils;
 
-use std::{env, process::exit};
+use std::process::exit;
 
-use clap::{App, Arg};
 #[cfg(debug_assertions)]
 use dotenv::dotenv;
 
@@ -12,32 +12,10 @@ fn main() {
     #[cfg(debug_assertions)]
     dotenv().ok();
 
-    let matches = App::new("OwlDict")
-        .version("0.1")
-        .author("Spyros Roum <spyrosr1@gmail.com>")
-        .about("A simple command line dictionary")
-        .arg(
-            Arg::with_name("word")
-                .help("The word to look for")
-                .required(true)
-                .index(1),
-        )
-        .get_matches();
-    let word = matches.value_of("word").unwrap();
-    let token = match env::var("OWLDICT_TOKEN") {
-        Ok(s) => s,
-        Err(_) => {
-            eprintln!("Please set your owldict token environment variable.");
-            exit(1);
-        }
-    };
-    let token = if !token.starts_with("Token ") {
-        "Token ".to_owned() + &token
-    } else {
-        token
-    };
+    let token = utils::get_token();
 
-    let word_url = OWL_URL.to_owned() + word;
+    let word = utils::get_word();
+    let word_url = OWL_URL.to_owned() + &word;
 
     let r = ureq::get(&word_url).set("Authorization", &token).call();
 
---
+++ src/utils.rs
@@ -0,0 +1,33 @@
+use std::{env, process::exit};
+
+use clap::{App, Arg};
+
+pub(crate) fn get_word() -> String {
+    let matches = App::new("OwlDict")
+        .version("0.1")
+        .author("Spyros Roum <spyrosr1@gmail.com>")
+        .about("A simple command line dictionary")
+        .arg(
+            Arg::with_name("word")
+                .help("The word to look for")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+    matches.value_of("word").unwrap().to_owned()
+}
+
+pub(crate) fn get_token() -> String {
+    let token = match env::var("OWLDICT_TOKEN") {
+        Ok(s) => s,
+        Err(_) => {
+            eprintln!("Please set your owldict token environment variable.");
+            exit(1);
+        }
+    };
+    if !token.starts_with("Token ") {
+        "Token ".to_owned() + &token
+    } else {
+        token
+    }
+}
